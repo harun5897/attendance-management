57cfd18a0b7ebb4332f7a34be5ebafd84a92ee37
Harun <harungb025@gmail.com>
2024-12-11 08:22:18 +0700

    create method for fetching api user and create form for edit user

--- views/user.php
+++ views/user.php
@@ -27,11 +27,11 @@
                 <div id="content-page-views" class="px-5 py-2">
                     <h3>Data Pengguna</h3>
                     <hr>
-                    <button type="button" class="btn btn-primary btn-sm mb-2" data-bs-toggle="modal" data-bs-target="#modal-user">
+                    <button type="button" class="btn btn-primary btn-sm mb-2" data-bs-toggle="modal" data-bs-target="#modal-create-user">
                         Tambah User
                     </button>
                     <!-- Modal Create User -->
-                    <div class="modal fade" id="modal-user" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal fade" id="modal-create-user" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header bg-secondary text-white">
@@ -50,26 +50,63 @@
                                         </select>
                                 </div>
                                 <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                                    <button type="submit" class="btn btn-primary" onclick="saveUser(event)">Simpan</button>
+                                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batal</button>
+                                    <button type="submit" class="btn btn-secondary" onclick="saveUser(event)">Simpan</button>
                                     </form>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <table class="table table-bordered table-sm shadow-lg" style="font-size: 14px !important;">
+                    <!-- Modal Edit User -->
+                    <div class="modal fade" id="modal-edit-user" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header bg-warning">
+                                    <h5 class="modal-title" id="exampleModalLabel">Form Edit User</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="" onsubmit="return updateUser(event)">
+                                        <input id="edit-id-user" type="hidden">
+                                        <input id="edit-username" class="form-control mb-3" type="text" placeholder="Masukan username" aria-label="username">
+                                        <input id="edit-email" class="form-control mb-3" type="text" placeholder="Masukan email" aria-label="email">
+                                        <select id="edit-role" class="form-select" aria-label="Default select example">
+                                            <option value="">Pilih Role</option>
+                                            <option value="admin">Admin</option>
+                                            <option value="leader">Leader</option>
+                                            <option value="manager">Manager</option>
+                                        </select>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batal</button>
+                                    <button type="submit" class="btn btn-warning" onclick="updateUser(event)">Simpan</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <table class="table table-bordered table-sm shadow-lg" style="font-size: 13px !important;">
                         <thead>
                             <tr>
                                 <th scope="col" class="text-center">No</th>
                                 <th scope="col">Username</th>
                                 <th scope="col">Email</th>
                                 <th scope="col">Role</th>
-                                <th scope="col" style="width: 300px;">Action</th>
+                                <th scope="col" style="width: 280px;">Action</th>
                             </tr>
                         </thead>
                         <tbody id="user-table-body">
                         </tbody>
                     </table>
+                    <nav aria-label="Page navigation example">
+                        <input type="hidden" id="total-pages">
+                        <input type="hidden" id="current-page" value="1">
+                        <ul class="pagination">
+                            <li class="page-item"><a class="page-link" href="#" onclick="getUser('prev')">Previous</a></li>
+                            <li class="page-item"><span id="display-current-page" class="page-link">1</span></li>
+                            <li class="page-item"><a class="page-link" href="#" onclick="getUser('next')">Next</a></li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </div>
@@ -81,12 +118,24 @@
 
 <script>
     getUser()
-    async function getUser() {
-        const responseGetUser = await fetch('/attendance/api/user.php/user', {
-            method: 'GET',
+    async function getUser(navigationPage) {
+        const totalPages = document.getElementById('total-pages').value;
+        const currentPage = document.getElementById('current-page').value;
+        let page = currentPage;
+        if(navigationPage == 'prev' && currentPage > 1) {
+            page = parseInt(currentPage) - 1;
+        }
+        if(navigationPage == 'next' && currentPage < totalPages) {
+            page = parseInt(currentPage) + 1;
+        }
+        const responseGetUser = await fetch('/attendance/api/user.php/get-user', {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
+            body: JSON.stringify({
+                page: page,
+            })
         }).then(response => response.json())
         let html_data = '';
         if (responseGetUser.data.length == 0 || !responseGetUser.success) {
@@ -94,20 +143,25 @@
             document.getElementById('user-table-body').innerHTML = html_data;
             return
         }
+        const dataPerPage = responseGetUser.pagination.data_per_page;
         responseGetUser.data.forEach((user, index) => {
+            const rowNumber = (page - 1) * dataPerPage + (index + 1);
             html_data += '<tr>';
-            html_data += `<th scope="row" class="text-center">${index + 1}</th>`;
+            html_data += `<th scope="row" class="text-center">${rowNumber}</th>`;
             html_data += `<td>${user.username}</td>`;
             html_data += `<td>${user.email}</td>`;
             html_data += `<td><span class="badge text-bg-success">${user.role}</span></td>`;
             html_data += '<td>';
-            html_data += `<button type="button" class="btn btn-sm btn-warning me-1">Edit</button>`;
-            html_data += `<button type="button" class="btn btn-sm btn-danger me-1" onclick="deleteUser(${user.id_user})">Hapus</button>`;
-            html_data += `<button type="button" class="btn btn-sm btn-info">Reset Password</button>`;
+            html_data += `<button type="button" class="btn btn-sm btn-warning me-1" style="font-size: 12px;" onclick="getUserDetail(${user.id_user})">Edit</button>`;
+            html_data += `<button type="button" class="btn btn-sm btn-danger me-1" style="font-size: 12px;" onclick="deleteUser(${user.id_user})">Hapus</button>`;
+            html_data += `<button type="button" class="btn btn-sm btn-info" style="font-size: 12px;" onclick="resetPassword(${user.id_user})">Reset Password</button>`;
             html_data += '</td>';
             html_data += '</tr>';
         });
         document.getElementById('user-table-body').innerHTML = html_data;
+        document.getElementById('current-page').value = responseGetUser.pagination.current_page;
+        document.getElementById('display-current-page').innerHTML = responseGetUser.pagination.current_page;
+        document.getElementById('total-pages').value = responseGetUser.pagination.total_pages;
     }
     async function saveUser(event) {
         event.preventDefault();
@@ -124,7 +178,7 @@
         if(!role) {
             return SwalAlert.warning('Data tidak lengkap!', 'Data role wajib di isi.')
         }
-        const responseSaveUser = await fetch('/attendance/api/user.php/user', {
+        const responseSaveUser = await fetch('/attendance/api/user.php/create-user', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -148,7 +202,7 @@
         if(!idUser) {
             return SwalAlert.warning('Terjadi kesalahan!', 'ID user tidak ditemukan')
         }
-        const responseDeleteUser = await fetch('/attendance/api/user.php/user', {
+        const responseDeleteUser = await fetch('/attendance/api/user.php/delete-user', {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json'
@@ -165,6 +219,91 @@
             Swal.close()
             window.location.href = '/attendance/views/user.php';
         }, 1000);
+    }
+    async function getUserDetail(idUser) {
+        if(!idUser) {
+            return SwalAlert.warning('Terjadi kesalahan!', 'ID user tidak ditemukan')
+        }
+        const responseDetailUser = await fetch('/attendance/api/user.php/detail-user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                idUser: idUser,
+            })
+        }).then(response => response.json())
+        if(!responseDetailUser.success) {
+            return SwalAlert.warning('Terjadi kesalahan', responseDetailUser.message)
+        }
+        const myModal = new bootstrap.Modal(document.getElementById("modal-edit-user"));
+        myModal.show();
+        const detailUser = responseDetailUser.data
+        document.getElementById('edit-id-user').value = detailUser.id_user
+        document.getElementById('edit-username').value = detailUser.username
+        document.getElementById('edit-email').value = detailUser.email
+        document.getElementById('edit-role').value = detailUser.role
+    }
+    async function updateUser(event) {
+        event.preventDefault();
+        const idUser = document.getElementById('edit-id-user').value
+        const username = document.getElementById('edit-username').value
+        const email = document.getElementById('edit-email').value
+        const role =  document.getElementById('edit-role').value
+
+        if(!idUser) {
+            return SwalAlert.warning('Terjadi kesalahan!', 'ID tidak ditemukan.')
+        }
+        if(!username) {
+            return SwalAlert.warning('Data tidak lengkap!', 'Data username wajib di isi.')
+        }
+        if(!email) {
+            return SwalAlert.warning('Data tidak lengkap!', 'Data email wajib di isi.')
+        }
+        if(!role) {
+            return SwalAlert.warning('Data tidak lengkap!', 'Data role wajib di isi.')
+        }
+        const responseUpdateUser = await fetch('/attendance/api/user.php/update-user', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                idUser: idUser,
+                username: username,
+                email: email,
+                role: role
+            })
+        }).then(response => response.json())
+        if(!responseUpdateUser.success) {
+            return SwalAlert.warning('Terjadi kesalahan', responseUpdateUser.message)
+        }
+        const modalElement = document.getElementById("modal-edit-user");
+        const modal = bootstrap.Modal.getInstance(modalElement);
+        modal.hide();
+        SwalAlert.success(responseUpdateUser.message)
+        setTimeout(() => {
+            Swal.close()
+            window.location.href = '/attendance/views/user.php';
+        }, 1000);
+    }
+    async function resetPassword(idUser) {
+        if(!idUser) {
+            return SwalAlert.warning('Terjadi kesalahan!', 'ID tidak ditemukan.')
+        }
+        const responseResetPassword = await fetch('/attendance/api/user.php/reset-password', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                idUser: idUser,
+            })
+        }).then(response => response.json())
+        if(!responseResetPassword.success) {
+            return SwalAlert.warning('Terjadi kesalahan', responseResetPassword.message)
+        }
+        SwalAlert.success(responseResetPassword.message)
     }
 </script>
 </body>
